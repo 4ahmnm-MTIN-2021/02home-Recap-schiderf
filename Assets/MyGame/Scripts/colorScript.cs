f60c33ca6fe80f710a155ef4cdde3b3a537bbc83
schiderf <felix.schider02@htl-salzburg.ac.at>
2020-10-05 15:43:01 +0200

    update an finish color scene

--- Assets/MyGame/Scripts/colorScript.cs
+++ Assets/MyGame/Scripts/colorScript.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
@@ -15,15 +13,13 @@ public class colorScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        void Start()
-        {
-            orangeGelb = new Color(1.0f, 205 / 255f, 25 / 255f);
-            kastanienBraun = new Color(95 / 255f, 34 / 255f, 0.0f);
-            sonnenuntergang = new Color(207 / 255f, 63 / 255f, 21 / 255f);
-            violettGrau = new Color(102 / 255f, 47 / 255f, 84 / 255f);
-            colors = new Color[] { Color.red, orangeGelb, kastanienBraun, sonnenuntergang, violettGrau };
-            img.GetComponent<Image>().color = colors[0];
-        }
+        orangeGelb = new Color(1.0f, 205 / 255f, 25 / 255f);
+        kastanienBraun = new Color(95 / 255f, 34 / 255f, 0.0f);
+        sonnenuntergang = new Color(207 / 255f, 63 / 255f, 21 / 255f);
+        violettGrau = new Color(102 / 255f, 47 / 255f, 84 / 255f);
+        colors = new Color[] { Color.red, orangeGelb, kastanienBraun, sonnenuntergang, violettGrau };
+        img.GetComponent<Image>().color = colors[0];
+
     }
 
     // Update is called once per frame
